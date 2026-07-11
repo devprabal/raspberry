2fbe3f8d85307eda96b008e1f8063a6700c9063f
devprabal <ghjxvnjd@gmail.com>
2026-07-11 14:42:15 +0530

    add sparkyfitness service

--- home/backup.sh
+++ home/backup.sh
@@ -32,3 +32,6 @@ mkdir -p linkding-db
 cp ~/linkding-db/db.sqlite3 ./linkding-db/db.sqlite3
 cp ~/linkding-db/tasks.sqlite3 ./linkding-db/tasks.sqlite3
 
+## sparkyfitness
+mkdir -p sparkyfitness-data
+cp -r ~/sparkyfitness-data/compose.yml ./sparkyfitness-data/compose.yml
